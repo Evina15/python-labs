6f4ab70c87c2301a9c0283e040ef9f21f03c53b5
Budhi Arta <budhiarta@Budhis-MacBook-Pro-2.local>
2020-01-22 00:39:30 +0800

    Updating but not finish

---
+++ python_miniprojects/sentence_analysis/sentence_analysis.py
@@ -0,0 +1,14 @@
+sentence = input("What is your favourite phrase? ")
+
+
+# check and count if the sentence is upper case
+print("Upper case: ", sum(1 for c in sentence if c.isupper()))
+
+# check and count if the sentence is lower case
+print("Lower case: ", sum(1 for c in sentence if c.islower()))
+
+# check and count the number of punctuation
+
+
+# count total number of characters
+
