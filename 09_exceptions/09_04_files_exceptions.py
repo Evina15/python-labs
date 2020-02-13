0e97cf8221b7dee3ce215286917194020302ec63
Budhi Arta <budhiarta@Budhis-MacBook-Pro-2.local>
2020-02-13 18:48:16 +0800

    Submitting 09_04

--- 09_exceptions/09_04_files_exceptions.py
+++ 09_exceptions/09_04_files_exceptions.py
@@ -23,3 +23,70 @@ BONUS CHALLENGE: write a custom Exception that inherits from Exception and raise
 first 100 characters of any of the files contain the string "Prince".
 
 '''
+
+# Open war_and_peace.txt, read the whole file content and store it in a variable
+var_war_peace = []
+with open("/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/war_and_peace.txt", "r") as warpeace:
+    for word in warpeace.readlines():
+        word = word.strip()
+        if word != "":
+            var_war_peace.append(word)
+
+# Open crime_and_punishment.txt and overwrite the whole content with an empty string
+with open("/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/crime_and_punishment.txt", "w") as crimepunishment:
+    var_crime_punishment = crimepunishment.write("")
+
+var_pride_prejudice = []
+with open("/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/pride_and_prejudice.txt", "r") as prideprejudice:
+    for word in prideprejudice.readlines():
+        word = word.strip()
+        if word != "":
+            var_pride_prejudice.append(word)
+
+# Loop over all three files and print out only the first character each
+wp = "/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/war_and_peace.txt"
+pp = "/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/pride_and_prejudice.txt"
+cp = "/Users/budhiarta/Documents/CodingNomads/labs/python_fundamentals-master/09_exceptions/books/crime_and_punishment.txt"
+
+
+for x in [wp, pp, cp]:
+    with open(x, "r") as f:
+        first_line = f.readline()
+
+        try:
+            print(first_line[0])
+
+        except IndexError as ie:
+            print("Cannot print the first character in war and peace, pride and prejudice, crime and punishment because ", ie)
+        except TypeError as te:
+            print("Cannot print the first character in war and peace, pride and prejudice, crime and punishment because ", te)
+
+
+
+# custom exception
+
+#class WordPrince(Exception):
+    #def __init__(self, message):
+        #super().__init__(message)
+
+#words = []
+#for x in [wp, pp, cp]:
+    #with open(x, "r") as f:
+        #for word in f.readlines():
+            #words = word.strip()
+
+
+#for p in words:
+    #for prince in range(0, 101):
+        #prince_words = prince.startswith("Prince")
+        #print(prince_words)
+
+
+
+
+
+
+
+
+
+# try:
