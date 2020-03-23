290232520f75794de5854954dce1af79fbcd4328
Budhi Arta <budhiarta@Budhis-MacBook-Pro-2.local>
2020-03-23 21:40:39 +0800

    Submitting 16

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def sum_(*args):
+    result = sum(args)
+    print(result)
+
+sum_(10, 10, 20)
+
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def my_kwargs(**kwargs):
+    for k, v in kwargs.items():
+        print(f"This {k} belongs to {v}")
+
+my_kwargs(carrot="rabbit", grass="cow", pizza="human")
+
